84315e65eff42bbcbca84fa9fe74a81d53228642
articicetea <jlair1@uncc.edu>
2021-12-10 04:34:59 -0500

    updating website

---
+++ itis3135/project/script/smiths_custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	/*index page slider */
+    $("#slider").bxSlider({
+		randomStart: true,
+        auto: true,
+		speed: 3000,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 750,
+        slideMargin: 20,
+		pager: true,
+		pagerType: 'short'
+    });
+	
+
+});
