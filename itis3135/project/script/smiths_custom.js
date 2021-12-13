c2b481035f468b6803c5407873a7cd448fff2174
articicetea <jlair1@uncc.edu>
2021-12-12 22:40:20 -0500

    updating website

--- itis3135/project/script/smiths_custom.js
+++ itis3135/project/script/smiths_custom.js
@@ -12,5 +12,46 @@ $(document).ready(function() {
 		pagerType: 'short'
     });
 	
+	/*Jquery checkbox radio widget*/
+	$("input:checkbox").on('click', function() {
+	  var $box = $(this);
+	  if ($box.is(":checked")) {
+		var group = "input:checkbox[name='" + $box.attr("name") + "']";
+		$(group).prop("checked", false);
+		$box.prop("checked", true);
+	  } else {
+		$box.prop("checked", false);
+	  }
+	});
+	
+	/*Submit rating onclick funciton*/
+	$( "#submitRating" ).click(function() {
+		$( this ).slideUp();
+		var $rating = $( '#googleRating' );
+		var counter = 4;
+		counter++;
+		$rating.text( counter + ' Google Ratings' );
+		$('#leaveReview').text('Thanks for your review!');
+	});
+	
+		/*Login jquery*/
+	$( "#loginButton" ).click(function() {
+		var getText = $('#loginUser').val();
+		console.log(getText);
+		$('#changeDiv').html( '<span>' + 'Thanks for logging in ' + getText + '</span>');
+	});
+	
+	/*FAQ Accordion */
+	$( function() {
+		$( "#accordion" ).accordion();
+	});
+	
+	
+	/*Submit design onclick funciton*/
+	$( "#submitDesign" ).click(function() {
+		$( this ).slideUp();
+		$('#thankyouDiv').text('Thanks for your design, we will contact you shortly');
+	});
+
 
 });
