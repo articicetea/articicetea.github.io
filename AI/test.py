4a9048ee421a3479c65c4e4fed3873eec7543d63
articicetea <jlair1@uncc.edu>
2022-02-24 21:51:25 -0500

    t

---
+++ AI/test.py
@@ -0,0 +1,23 @@
+import random
+
+board = [8,8]
+testBoard = [8,8]
+
+heur = 0
+queenPosition = 0
+restart = 0
+numMoves = 0
+newBoard = True
+neighbor = 8
+
+for i in range (0,8):
+    for j in range(0,8):
+        board[i,j]
+
+def randomBoard():
+    while(queenPosition < 8):
+        for k in range(0,8):
+
+
+
+
